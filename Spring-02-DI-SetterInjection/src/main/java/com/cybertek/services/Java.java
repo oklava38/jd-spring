ff82c985960e256ec786a72325e7f07c07ea94b5
hasangokbel <gokbelhasan@gmail.comdd>
2020-10-20 19:23:33 -0500

    Setter Injection

---
+++ Spring-02-DI-SetterInjection/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,17 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+import lombok.Data;
+
+@Data
+public class Java implements Course {
+
+    private ExtraSessions extraSessions;
+
+
+
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours : " + (20 + extraSessions.getHours() ));
+    }
+}
